a3be3a6efe1c5811a81992bd0d2543c3cb0facbf
Chance Zibolsk <chance.zibolski@gmail.com>
2012-11-10 19:33:55 -0800

    Now has example data for videos and music.

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, url_for, request
 import MySQLdb as mdb
 
 app = Flask(__name__)
@@ -15,13 +15,21 @@ class Artist(object):
     def __init__(self, f_name, l_name):
         self.f_name = f_name
         self.l_name = l_name
+        self.full_name = f_name + " " + l_name
 
 class Song(object):
-    def __init__(self, title, artist, category, length):
+    def __init__(self, title, artist, album, category, length):
         self.title = title
         self.artist = artist
+        self.album = album
         self.category = category
         self.length = length
+
+class Album(object):
+    def __init__(self, title, num_songs, songs):
+        self.title = title
+        self.num_songs = num_songs
+        self.songs = songs
 
 def connect_db():
     return mdb.connect(host="localhost", port=3306, user="testuser",
@@ -38,8 +46,8 @@ def teardown_request(exception):
 
 
 
-@app.route('/')
 @app.route('/home/')
+@app.route('/')
 def home():
     return render_template("home.html")
 
@@ -50,19 +58,38 @@ def get_movies():
 
     return movies
 
-def get_music():
-    music = []
+def get_songs():
+    aSong = Song("Dear Prudence","The Beatles","The Beatles","Rock",3.56*60)
+    songs = [aSong]
+    return songs
 
-    return music
+def get_artists():
+    artists = ['Test1','Tester2','Some Unknown Artist']
+    return artists
+
+def get_movie_genres():
+    genres = ["action", "adventure", "comedy", "crime", "horror"]
+    return genres
 
 @app.route('/videos/')
 def videos():
     movies = get_movies()
-    return render_template("videos.html", movies=movies)
+    genres = get_movie_genres()
+    return render_template("videos.html", movies=movies, genres=genres)
 
 @app.route('/music/')
 def music():
-    return render_template("music.html")
+    artists = get_artists()
+    songs = get_songs()
+    return render_template("music.html", artists=artists, songs=songs)
+
+@app.route('/add_video', methods=['POST','GET'])
+def add_video():
+    return "Not yet implemented"
+
+@app.route('/add_music', methods=['POST','GET'])
+def add_music():
+    return "Not yet implemented"
 
 
 if __name__ == '__main__':
