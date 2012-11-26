27f1ed42f6b452e0eb28e0f0008261688c99cc81
Chance Zibolsk <chance.zibolski@gmail.com>
2012-11-25 21:45:12 -0800

    Fully working insertions, all templates and required items finished

--- myapp.py
+++ myapp.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, g, url_for, request
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify, json
 from itertools import groupby
 import MySQLdb as mdb
 import MySQLdb.cursors
 
+DEBUG=True
+SECRET_KEY = 'development key'
+
 app = Flask(__name__)
-app.debug = True
+app.config.from_object(__name__)
 
 def connect_db():
     return mdb.connect(host="localhost", port=3306, user="testuser",
@@ -36,6 +40,185 @@ def get_movie_genres():
     genres = ["action", "adventure", "comedy", "crime", "horror"]
     return genres
 
+def get_band(band_id):
+    query = """SELECT band_id as id, name, year_started AS begin,
+               year_ended AS end, website
+               FROM band WHERE band_id = %s"""
+
+    cursor = g.db.cursor()
+    cursor.execute(query, band_id)
+    band = cursor.fetchone()
+
+    return band
+
+def get_bands():
+    band_query = """SELECT b.band_id, b.name, b.year_started, b.year_ended,
+                    b.website
+                    FROM band b
+                    GROUP BY b.name
+                    ORDER BY b.name"""
+
+    #grab all bands
+    cursor = g.db.cursor()
+    cursor.execute(band_query)
+
+    row_bands = cursor.fetchall()
+
+    bands = []
+    for band in row_bands:
+        aBand = dict(id=band['band_id'], name=band['name'],
+        begin=band['year_started'], end=band['year_ended'],
+        website=band['website'],
+        members=[], albums=[], labels=[], genres=[])
+
+        # If 0, then band is currently active
+        if aBand['end'] == 0:
+            aBand['end'] = "Present"
+
+        # Grab band members for band
+        aBand['members'] = get_members(aBand['id'])
+        aBand['albums'] = get_band_albums(aBand['id'])
+
+        for album in aBand['albums']:
+            # Add genres and labels to band, if not already added
+            genre = album['genre']
+            label = album['label']
+            if genre not in aBand['genres']:
+                aBand['genres'].append(genre)
+            if label not in aBand['labels']:
+                aBand['labels'].append(label)
+
+        # Add band to list of bandj
+        bands.append(aBand)
+
+    return bands
+
+def get_members(band_id):
+    member_query = """SELECT CONCAT(f_name, ' ', l_name) as name, a.artist_id
+                      FROM artist a
+                      INNER JOIN band_member bm ON bm.member = a.artist_id
+                      WHERE bm.band = %s"""
+
+    cursor = g.db.cursor()
+    cursor.execute(member_query, band_id)
+    row_members = cursor.fetchall()
+
+    members = []
+    for member in row_members:
+        a_name = member['name']
+        a_id = member['artist_id']
+        aMember = dict(id=a_id, name=a_name)
+        members.append(aMember)
+
+    return members
+
+def get_band_albums(band_id):
+    album_query = """SELECT a.album_id, a.name, a.record_label, a.genre,
+                     a.release_date
+                     FROM band_album ba
+                     LEFT JOIN album a ON a.album_id=ba.album
+                     WHERE ba.band = %s
+                     ORDER BY release_date"""
+
+    # Grab albums for the band
+    cursor = g.db.cursor()
+    cursor.execute(album_query, band_id)
+    row_albums = cursor.fetchall()
+
+    albums = []
+    for album in row_albums:
+        anAlbum = dict(id=album['album_id'], name=album['name'],
+            label=album['record_label'], genre=album['genre'],
+            release=album['release_date'])
+
+        anAlbum['songs'] = get_album_songs(anAlbum['id'])
+
+        albums.append(anAlbum)
+
+    return albums
+
+@app.route("/band/albums")
+def band_albums():
+    album_query = """SELECT a.album_id, a.name
+                     FROM band_album ba
+                     LEFT JOIN album a ON a.album_id=ba.album
+                     WHERE ba.band = %s
+                     ORDER BY a.name"""
+
+    band = request.args.get('band_id', 0, type=int)
+    if band:
+        cursor = g.db.cursor()
+        cursor.execute(album_query, band)
+        row_albums = cursor.fetchall()
+
+        albums = []
+        for album in row_albums:
+            albums.append(dict(id=album['album_id'], name=album['name']))
+
+        return jsonify(albums=albums)
+
+def get_artist(artist_id):
+    query = """SELECT CONCAT(a.f_name, ' ', a.l_name) as name, artist_id as id
+                FROM artist a
+                WHERE a.artist_id = %s"""
+
+    cursor = g.db.cursor()
+    cursor.execute(query, artist_id)
+    artist = cursor.fetch()
+
+    return artist
+
+def get_artists():
+    query = """SELECT CONCAT(a.f_name, ' ',a.l_name) as name, a.artist_id AS id
+               FROM artist a ORDER BY name"""
+
+    cursor = g.db.cursor()
+    cursor.execute(query)
+    artists = cursor.fetchall()
+
+    return artists
+
+def get_album_songs(album_id):
+    album_song = """SELECT s.song_id, s.name, s.length, alb.track_num
+                    FROM album_song alb
+                    LEFT JOIN song s ON s.song_id=alb.song
+                    WHERE alb.album=%s
+                    ORDER BY alb.track_num"""
+
+    cursor = g.db.cursor()
+    cursor.execute(album_song, album_id)
+    row_songs = cursor.fetchall()
+
+    songs = []
+    for song in row_songs:
+        aSong = dict(id=song['song_id'], name=song['name'],
+            length=song['length'], track_num=song['track_num'])
+
+        songs.append(aSong)
+
+    return songs
+
+def get_album(album_id):
+    album_query = """SELECT a.album_id, a.name, a.record_label, a.genre,
+                     a.release_date, b.name as band, b.band_id
+                     FROM band_album ba
+                     LEFT JOIN album a ON a.album_id=ba.album
+                     LEFT JOIN band b on b.band_id=ba.band
+                     WHERE ba.album = %s"""
+
+    # Grab albums for the band
+    cursor = g.db.cursor()
+    cursor.execute(album_query, album_id)
+    album_result = cursor.fetchone()
+
+    album = dict(id=album_result['album_id'], name=album_result['name'],
+        label=album_result['record_label'], genre=album_result['genre'],
+        release=album_result['release_date'], band=album_result['band'],
+        band_id=album_result['band_id'])
+
+    album['songs'] = get_album_songs(album_id)
+
+    return album
 
 @app.route('/home/')
 @app.route('/')
@@ -57,85 +240,218 @@ def get_movie(movie_id):
 
 @app.route('/music/')
 def music():
-    query = """SELECT b.band_id, b.name, b.year_started, b.year_ended, b.website,
-               a.artist_id, CONCAT(a.f_name, ' ',a.l_name) as artist
-               FROM band_member m
-               LEFT JOIN artist a ON m.member = a.artist_id
-               LEFT JOIN band b ON m.band = b.band_id
-               GROUP BY b.name, artist
-               ORDER BY b.band_id, a.artist_id"""
+    # Gather template data
+    bands = get_bands()
+    artists = get_artists()
 
-    album_query = """SELECT a.album_id, a.name, a.record_label, a.genre
-                     FROM band_album ba
-                     LEFT JOIN album a ON a.album_id=ba.album
-                     WHERE ba.band = %s"""
+    return render_template("music.html", bands=bands, artists=artists)
 
-    cur = g.db.cursor()
-    cur.execute(query)
-    row_bands = cur.fetchall()
+@app.route('/create/band', methods=['POST','GET'])
+def create_band():
+    name = request.form['band_name']
+    started = request.form['year_started']
+    ended = request.form['year_ended']
+    website = request.form['band_website']
 
-    bands = []
-    for band, members in groupby(row_bands, lambda row: row['band_id']):
-        first = True
-        for member in members:
-            # First iteration grabs band info
-            if first == True:
-                aBand = dict(id=member['band_id'], name=member['name'],
-                    begin=member['year_started'], end=member['year_ended'],
-                    website=member['website'], members=[], albums=[],
-                    labels=[], genres=[])
+    query = """INSERT INTO band(name, year_started, year_ended, website)
+               VALUES (%s, %s, %s, %s)"""
 
-                # Grab albums for the band
-                cur.execute(album_query, aBand['id'])
-                row_albums = cur.fetchall()
+    try:
+        cur = g.db.cursor()
+        cur.execute(query, (name, started, ended, website))
+        g.db.commit()
+    except mdb.Error, e:
+        print "An error has occurred. %s" % e
+        message = 'Error attempting to add band "%s"' % name
+        msg_type = 'error'
+    else:
+        message = 'Successfully added band "%s"' % name
+        msg_type = 'success'
 
-                for album in row_albums:
-                    anAlbum = dict(id=album['album_id'], name=album['name'])
-                    aBand['albums'].append(anAlbum)
+    flash(message, msg_type)
+    return redirect(url_for('music'))
 
-                    genre = album['genre']
-                    label = album['record_label']
-                    if genre not in aBand['genres']:
-                        aBand['genres'].append(genre)
-                    if label not in aBand['labels']:
-                        aBand['labels'].append(label)
+@app.route('/create/album', methods=['POST', 'GET'])
+def create_album():
+    # Check if any values are empty.
+    errors = False
+    for key, value in request.form.iteritems():
+        if value == '':
+            errors = True
+            if key == "band":
+                err_message = 'Error, must select an existing band!'
+            else:
+                err_message = 'Error, %s cannot be empty.' % key
+            flash(err_message, "error")
 
+    #if title in get_band_albums():
+    #    errors = True
+    #    err_message = 'Error, album "%s" already exists for band "%s"'
+    #    flash(err_message % (title, band), 'error')
 
-                if aBand['end'] == 0:
-                    aBand['end'] = "Present"
+    if not errors:
+        # Still needs error checking for if album exists. However albums are on
+        # their own table. Making checking harder. May want to adjust schema.
+        album_query = """INSERT INTO album(name, release_date, record_label, genre)
+                   VALUES (%s, %s, %s, %s)"""
 
-                first = False
+        band_album_query = """INSERT INTO band_album(band, album)
+                              SELECT %s, LAST_INSERT_ID()"""
 
-            # Add each member to the band's member dict, with their ID as key
-            a_name = member['artist']
-            a_id = member['artist_id']
-            aBand['members'].append({'id': a_id, 'name': a_name})
-        print aBand
+        cur = g.db.cursor()
+        band = request.form['band']
+        band = get_band(band)
+        title = request.form['title']
+        genre = request.form['genre']
+        label = request.form['label']
+        release = request.form['release']
 
-        bands.append(aBand)
+        try:
+            cur.execute(album_query, (title, release, label, genre))
+            cur.execute(band_album_query, band['id'])
+            g.db.commit()
+        except mdb.Error, e:
+            print "An error has occurred. %s" % e
+            message = 'Error attempting to add album "%s" to band "%s"' % \
+            (title, band['name'])
+            msg_type = 'error'
+        else:
+            message = 'Successfully added album "%s" for band "%s"' % \
+            (title, band['name'])
+            msg_type = 'success'
 
-    return render_template("music.html", bands=bands)
+        flash(message, msg_type)
+
+    return redirect(url_for('music'))
+
+@app.route('/create/artist', methods=['POST', 'GET'])
+def create_artist():
+    errors = False
+    for key, value in request.form.iteritems():
+        if value == '':
+            errors = True
+            err_message = 'Error, must enter %s name.' % key
+            flash(err_message, "error")
+
+    if not errors:
+        artist_query = """INSERT INTO artist(f_name, l_name)
+                          VALUES (%s, %s)"""
+
+        cur = g.db.cursor()
+        first = request.form['first']
+        last = request.form['last']
+        artist = (first, last)
+
+        try:
+            cur.execute(artist_query, artist)
+            g.db.commit()
+        except mdb.Error, e:
+            print "An error has occurred. %s" % e
+            message = 'Error attempting to add artist "%s %s"' % artist
+            msg_type = 'error'
+        else:
+            message = 'Successfully added artist "%s %s"' % artist
+            msg_type = 'success'
+
+        flash(message, msg_type)
+
+    return redirect(url_for('music'))
+
+@app.route('/create/song', methods=['POST', 'GET'])
+def create_song():
+    errors = False
+    for key, value in request.form.iteritems():
+        if value == '':
+            errors = True
+            err_message = 'Error, "%s" cannot be empty!' % key
+            flash(err_message, "error")
+
+    if not errors:
+        song_query = """INSERT INTO song(name, length)
+                        VALUES (%s, %s)"""
+        song_album_query = """INSERT INTO album_song(album, song, track_num)
+                              SELECT %s, LAST_INSERT_ID(), %s"""
+
+        cursor = g.db.cursor()
+        song = request.form['song']
+        album = request.form['album']
+        album = get_album(album)
+        length = request.form['length']
+        number = request.form['track_num']
+
+        try:
+            cursor.execute(song_query, (song, length))
+            cursor.execute(song_album_query, (album['id'], number))
+            g.db.commit()
+        except mdb.Error, e:
+            print "An error has occurred. %s" % e
+            message = 'Error attempting to add song "%s" to album "%s"' % \
+            (song, album['name'])
+            msg_type = 'error'
+        else:
+            message = 'Successfully added song "%s" to album "%s"' % \
+            (song, album['name'])
+            msg_type = 'success'
+
+        flash(message, msg_type)
+
+    return redirect(url_for('music'))
+
+@app.route('/add/artist', methods=['POST', 'GET'])
+def add_artist():
+    err_message = "Error, %s required"
+    error = False
+
+    b_id = request.form['band']
+    a_id = request.form['artist']
+    if b_id == '':
+        error = True
+        flash(err_message % "Band", "error")
+    if a_id == '':
+        error = True
+        flash(err_message % "Artist", "error")
+
+    if not error:
+        cur = g.db.cursor()
+
+        band = get_band(b_id)
+        artist = get_artist(a_id)
+
+        query = """INSERT INTO band_member(band, member) VALUES (%s, %s)"""
+
+        try:
+            cur.execute(query, (band['id'], artist['id']))
+            g.db.commit()
+        except mdb.Error, e:
+            print "An error has occurred. %s" % e
+                #1062
+            message = 'Error attempting to add artist "%s" to band "%s"' % \
+                (artist['name'], band['name'])
+            msg_type = 'error'
+        else:
+            message = 'Successfully added artist "%s" to band "%s"' % \
+                (artist['name'], band['name'])
+            msg_type = 'success'
+
+    flash(message, msg_type)
+
+    return redirect(url_for('music'))
+
 
 @app.route('/artist/<int:a_id>')
-def get_artist(a_id):
+def artist(a_id):
     return str(a_id)
 
 @app.route('/band/<int:b_id>')
-def get_band(b_id):
-    return str(b_id)
+def band(b_id):
+    band = get_band(b_id)
+    albums = get_band_albums(b_id)
+    return render_template("band.html", band=band, albums=albums)
 
 @app.route('/album/<int:alb_id>')
-def get_album(alb_id):
-    return str(alb_id)
-
-@app.route('/add_video', methods=['POST','GET'])
-def add_video():
-    return "Not yet implemented"
-
-@app.route('/add_music', methods=['POST','GET'])
-def add_music():
-    return "Not yet implemented"
-
+def album(alb_id):
+    album = get_album(alb_id)
+    return render_template("album.html", album=album)
 
 if __name__ == '__main__':
     app.run()
